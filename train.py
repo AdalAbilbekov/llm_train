06a65acedef387c23692a44fca4d7d15809588be
AdalAbilbekov <adalabilbekov@gmail.com>
2024-12-06 17:05:54 +0500

    Feat: Adding accelerate multi GPU training option. (Not tested yet).

--- train.py
+++ train.py
@@ -6,19 +6,22 @@ import fire
 import torch
 from torch.utils.data import DataLoader
 import tqdm
+from accelerate import Accelerator
 
 def main(config_path):
     conf = OmegaConf.load(config_path)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
     tokenizer, model, optimizer = load_model(conf)
+
+    if conf.enable_fsdp:
+        accelerator = Accelerator()
+    else:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model.to(device)
 
     dataset = build_custom_dataset(conf.dataset_path, tokenizer, conf.max_length, conf.dataset_type)
 
     dl_train = DataLoader(dataset, shuffle=False, batch_size=2)
-
-    model.to(device)
 
     global_step = 0
     epochs = 3
@@ -27,16 +30,22 @@ def main(config_path):
         model.train()
         pbar = tqdm.tqdm(colour="blue", desc=f"Training Epoch: {epoch+1}", total=steps_per_epoch, dynamic_ncols=True)
         for step, batch in enumerate(dl_train):
-            batch = {k: v.to(device) for k, v in batch.items()}
+            if not conf.enable_fsdp:
+                batch = {k: v.to(device) for k, v in batch.items()}
+            
             loss = model(**batch).loss
-            loss.backward()
+
+            if conf.enable_fsdp:
+                accelerator.backward(loss)
+            else:
+                loss.backward()
             optimizer.step()
             optimizer.zero_grad()
             pbar.update()
             pbar.set_description(f"Training Epoch: {epoch}, step {step}/{steps_per_epoch} completed (loss: {loss.detach().float()})")
 
             global_step +=1
-            
+
             if global_step % conf.save_step == 0:
                 print(f"Saving a checkpont {conf.save_path}_{global_step}")
                 model.save_pretrained(f"{conf.save_path}_{global_step}", from_pt=True)
