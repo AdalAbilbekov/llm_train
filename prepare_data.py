a780cb2d4d0963a97f93a445d33f91aa6e93bdba
AdalAbilbekov <adalabilbekov@gmail.com>
2024-12-06 16:25:38 +0500

    Feat: Tokenization and template prep. is done within CustomDataset as methods. Fixed the correct way of applying IGNORE_CEL_INDEX to labels. Fix: replacing pad_token to eos/eot token fixed the problem with the vocab_size and embedding_size.

--- prepare_data.py
+++ prepare_data.py
@@ -1,53 +1,68 @@
-from datasets import load_dataset
+from typing import List
+from datasets import load_dataset, DatasetDict
+from transformers.tokenization_utils_base import BatchEncoding
 import torch
 from torch.utils.data import Dataset
 from transformers import AutoTokenizer
 import pdb
 
 class DatasetTokenization(Dataset):
-    def __init__(self, input_ids, attention_mask):
-        self.input_ids = input_ids
-        self.attention_mask = attention_mask
+    def __init__(self, labels, input_ids, instruction, tokenizer, max_length):
+        self.inputs = input_ids
+        self.instructions = instruction
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_length = max_length
 
-    def __len__(self):
-        return len(self.input_ids)
+    def __len__(self) -> int:
+        return len(self.inputs)
+    
+    def prepare_prompt(self, input, label) -> str | List[int] | List[str] | List[List[int]] | BatchEncoding:
+
+        messages = [
+            {"role": "user", "content": f"{input}"},
+            {
+                "role": "assistant",
+                "content": f"{label}",
+            },
+        ]
+
+        input_text = self.tokenizer.apply_chat_template(messages, tokenize=False).strip()
+        encoded_x = self.tokenizer(input_text, padding="max_length", truncation=True, max_length=self.max_length)
+        encoded_y = self.tokenizer.encode(label + self.tokenizer.eos_token)
+
+        labels_ignore_idx = len(encoded_x["input_ids"][:sum(encoded_x["attention_mask"])]) - len(self.tokenizer.encode(label + self.tokenizer.eos_token))
+        if 0 in encoded_x["attention_mask"]:
+            encoded_y = [-100]*labels_ignore_idx + encoded_y + [-100]*(len(encoded_x["attention_mask"])-encoded_x["attention_mask"].index(0))
+        else:
+            encoded_y = [-100]*labels_ignore_idx + encoded_y
+
+        
+
+        return encoded_x['input_ids'][:self.max_length], encoded_y[:self.max_length], encoded_x['attention_mask'][:self.max_length]
 
     def __getitem__(self, idx):
-        input_tokens = torch.tensor(self.input_ids[idx])
-        attention_mask = torch.tensor(self.attention_mask[idx])
-        labels = input_tokens.clone()
+        input = " ".join([self.inputs[idx], self.instructions[idx]]).strip()
+        label = self.labels[idx].strip()
+        input_ids, labels, mask = self.prepare_prompt(input, label)
+        
         return {
-            "input_ids":input_tokens,
-            "attention_mask":attention_mask,
-            "labels":labels
+            "input_ids":torch.tensor(input_ids),
+            "attention_mask": torch.tensor(mask),
+            "labels":torch.tensor(labels)
         }
 
-def prepare_prompt(sample, tokenizer):
-    messages = [
-        {"role": "user", "content": f"{sample["input"]} {sample["instruction"]}"},
-        {
-            "role": "assistant",
-            "content": f"{sample["output"]}",
-        },
-    ]
-
-    input_text = tokenizer.apply_chat_template(messages, tokenize=False)
-    sample['prompt'] = input_text
-    return sample
 
 def tokenize_function(examples, tokenizer, max_length):
     return tokenizer(examples["prompt"], padding="max_length", truncation=True, max_length=max_length)
 
-def build_custom_dataset(dataset_path, tokenizer, max_length):
-    dataset = load_dataset("json", data_files=dataset_path)
+def build_custom_dataset(dataset_path, tokenizer, max_length, dataset_type):
+    dataset = load_dataset(dataset_type, data_files=dataset_path)
+    dataset = dataset['train']
 
-    ds = dataset.map(lambda x: prepare_prompt(x, tokenizer), num_proc=8)
+    tokenizer.pad_token_id = tokenizer.eos_token_id
 
-    tokenizer.add_special_tokens({'pad_token': '<pad>'})
-
-    ds = ds.map(lambda x: tokenize_function(x, tokenizer, max_length), num_proc=8)
-
-    dataset_train = DatasetTokenization(ds['train']['input_ids'], ds['train']['attention_mask'])
+    dataset_train = DatasetTokenization(dataset['output'], dataset['input'], dataset['instruction'], tokenizer, max_length)
 
     return dataset_train
 
