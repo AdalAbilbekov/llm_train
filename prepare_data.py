864a8f7a040c314eb0623e82673628db8a4b1727
AdalAbilbekov <adalabilbekov@gmail.com>
2024-12-06 08:22:09 +0000

    Fix: Column name was changed to <input_ids>, added max_length of the input sequence.

--- prepare_data.py
+++ prepare_data.py
@@ -17,8 +17,8 @@ class DatasetTokenization(Dataset):
         attention_mask = torch.tensor(self.attention_mask[idx])
         labels = input_tokens.clone()
         return {
-            "input_tokens":input_tokens,
-            "mask":attention_mask,
+            "input_ids":input_tokens,
+            "attention_mask":attention_mask,
             "labels":labels
         }
 
@@ -35,24 +35,24 @@ def prepare_prompt(sample, tokenizer):
     sample['prompt'] = input_text
     return sample
 
-def tokenize_function(examples, tokenizer):
-    return tokenizer(examples["prompt"], padding="max_length", truncation=True, max_length=256)
+def tokenize_function(examples, tokenizer, max_length):
+    return tokenizer(examples["prompt"], padding="max_length", truncation=True, max_length=max_length)
 
-def build_custom_dataset(dataset_path, tokenizer):
+def build_custom_dataset(dataset_path, tokenizer, max_length):
     dataset = load_dataset("json", data_files=dataset_path)
 
     ds = dataset.map(lambda x: prepare_prompt(x, tokenizer), num_proc=8)
 
     tokenizer.add_special_tokens({'pad_token': '<pad>'})
 
-    ds = ds.map(lambda x: tokenize_function(x, tokenizer), num_proc=8)
+    ds = ds.map(lambda x: tokenize_function(x, tokenizer, max_length), num_proc=8)
 
     dataset_train = DatasetTokenization(ds['train']['input_ids'], ds['train']['attention_mask'])
 
     return dataset_train
 
 if __name__=="__main__":
-    tokenizer_path = "/data/nvme7n1p1/mistral_models/models--mistralai--Mixtral-8x7B-Instruct-v0.1/snapshots/Mixtral-8x7B-Instruct-v0.1"
+    tokenizer_path = "HuggingFaceTB/SmolLM2-135M-Instruct"
     dataset_path = "/data/nvme7n1p1/critical_all/critical_instruct/2024.09.06_instruct_kk_benchmarks-arc_119687.json"
 
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
