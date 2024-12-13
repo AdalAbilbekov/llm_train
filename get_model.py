2ca19d1deefc2cbc50b0e5900c563b870a2f39b7
Adal <adalabilbekov@gmail.com>
2024-12-13 15:45:37 +0600

    Feat: FSDP sharding among multiple GPU has been added, TODO: setup optimizer for dist. training.

--- get_model.py
+++ get_model.py
@@ -8,34 +8,41 @@ import torch.optim as optim
 from torch.distributed.fsdp import (
     FullyShardedDataParallel as FSDP,
 )
-from policies import get_policies
+from policies import (
+    get_policies, 
+    apply_fsdp_checkpointing,
+    setup,
+    clear_gpu_cache
+    )
 
 from torch.distributed.fsdp.fully_sharded_data_parallel import CPUOffload
 from torch.distributed.fsdp import ShardingStrategy
 import torch.distributed as dist
 import os
 
-def setup():
-    """Initialize the process group for distributed training"""
-    dist.init_process_group("nccl")
+from time import sleep
 
-# TODO: Add wraping policy for sharding model via auto_wrap_policy and FSDP
 def set_model(model, model_config, rank):
     if model_config.enable_fsdp:
-        mixed_precision_policy, wrapping_policy = get_policies(model_config, rank)
+        mixed_precision_policy, wrapping_policy, to_use_bfloat = get_policies(model_config, rank)
+
+        # Use pure BF16
+        if model_config.brain_float and to_use_bfloat: model.to(torch.bfloat16)
+        
         model = FSDP(
             model,
-            auto_wrap_policy= wrapping_policy,
-            cpu_offload=CPUOffload(offload_params=True) if model_config.fsdp_cpu_offload else None,
-            mixed_precision=mixed_precision_policy,
+            auto_wrap_policy=wrapping_policy,
+            mixed_precision=mixed_precision_policy if not model_config.brain_float else None,
             sharding_strategy=ShardingStrategy.FULL_SHARD,
             device_id=torch.cuda.current_device(),
-            limit_all_gathers=True,
-            sync_module_states=False,
-            param_init_fn=None,
-            )
-        pdb.set_trace()
-        pass
+        )
+
+        apply_fsdp_checkpointing(model)
+        
+        if rank == 0:
+            print(model)
+
+        return model
     else:
         return model
 
@@ -53,6 +60,7 @@ def load(model_config: None):
 
     return model
 
+# TODO: Prepare optimizer for the FSDP train.
 def load_optimizer(model, model_config):
     return optim.AdamW(
         model.parameters(),
@@ -60,21 +68,26 @@ def load_optimizer(model, model_config):
         # weight_decay=model_config.weight_decay
     )
 
-# TODO: add model parallel sharding.
-def load_model(model_config):
+def load_model(model_config, rank):
     model = load(model_config)
-    setup()
-    rank = int(os.environ["RANK"])
     model = set_model(model, model_config, rank)
 
     tokenizer = AutoTokenizer.from_pretrained(model_config.model.path)
     tokenizer.pad_token_id = tokenizer.eos_token_id
 
-    optimizer = load_optimizer(model, model_config)
-
+    # optimizer = load_optimizer(model, model_config)
+    print("let's go to the next era")
+    if rank==0:
+        pdb.set_trace()
+    sleep(3000)
     return tokenizer, model, optimizer
-
     
 if __name__=="__main__":
     conf = OmegaConf.load("config.yaml")
-    tokenizer, model, optmizer = load_model(conf)
+    setup()
+    local_rank = int(os.environ["LOCAL_RANK"])
+    rank = int(os.environ["RANK"])
+    if torch.distributed.is_initialized(): #<- without this part all shards will be loaded to the single GPU.
+        torch.cuda.set_device(local_rank)
+        clear_gpu_cache(local_rank)
+    tokenizer, model, optmizer = load_model(conf, rank)
